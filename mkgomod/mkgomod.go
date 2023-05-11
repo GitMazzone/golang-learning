edab629130951c7fd7e3d986724d0f39d0521bc0
Michael Mazzone <mazzone.deca@gmail.com>
2023-05-10 21:58:54 -0400

    add basic server and make mkgomod better

--- mkgomod/mkgomod.go
+++ mkgomod/mkgomod.go
@@ -6,12 +6,33 @@ import (
 )
 
 func main() {
-	modName := "../" + os.Args[1]
+	modName := os.Args[1]
+	dirName := "../" + modName
 
-	err := os.Mkdir(modName, 0777)
+	err := os.Mkdir(dirName, 0777)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "mkdir: %s\n", err)
 		os.Exit(1)
 	}
+
+	file, err := os.Create(dirName + "/" + modName + ".go")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "os.Create: %s\n", err)
+		os.Exit(1)
+	}
+
+	file.WriteString(`package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("New module")
+}
+	`)
+
+	defer file.Close()
 }
